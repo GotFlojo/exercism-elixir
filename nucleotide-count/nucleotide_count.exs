104ce77c3bfb7b7be400276aafb24cd6a7239027
Florian Fischer <10535183+GotFlojo@users.noreply.github.com>
2018-09-22 20:44:19 +0200

    Add nucleotide solution
    Add primitive solution for histogram that iterates the strand anew for every
    nucleotide. This is commented out
    Add solution that iterates the strand while simultaneously incrementing
    the histogram map. Also this does some error handling if it accounters
    a nucleotide that is unexpected i.e. not in the histogram map.
    Add a test for the error handling

--- nucleotide-count/nucleotide_count.exs
+++ nucleotide-count/nucleotide_count.exs
@@ -13,7 +13,12 @@ defmodule NucleotideCount do
   1
   """
   @spec count([char], char) :: non_neg_integer
+  def count([], _), do: 0
   def count(strand, nucleotide) do
+    Enum.reduce(strand, 0, 
+      fn n, cnt ->
+        if n == nucleotide, do: (cnt + 1), else: cnt
+      end)
   end
 
   @doc """
@@ -24,7 +29,29 @@ defmodule NucleotideCount do
   iex> NucleotideCount.histogram('AATAA')
   %{?A => 4, ?T => 1, ?C => 0, ?G => 0}
   """
-  @spec histogram([char]) :: map
+  @spec histogram([char]) :: map | {:error, String.t()}
+  def histogram([]),  do: empty_nukelotide_histogram(@nucleotides) 
   def histogram(strand) do
+    empty_histogram = empty_nukelotide_histogram(@nucleotides)
+    Enum.reduce_while(strand, empty_histogram, 
+      fn nuc, hist -> 
+        case Map.fetch(hist, nuc) do
+          {:ok, val} -> {:cont, %{hist | nuc => val + 1}}
+          :error     -> {:halt, {:error, "#{[nuc]} is not recognized as a valid nucleotide"}}
+        end
+      end)
+  end
+  # Inefficient implmentation where we iterate the whole strand again for every
+  # nucleotide
+  # def histogram(strand) do
+    # empty_histogram = empty_nukelotide_histogram(@nucleotides)
+    # Enum.reduce(@nucleotides, empty_histogram,
+      # fn n, hist -> %{hist | n => count(strand, n)} end)
+  # end
+
+  @spec empty_nukelotide_histogram([char]) :: map
+  defp empty_nukelotide_histogram([]), do: %{}
+  defp empty_nukelotide_histogram(nucleotides) do
+    for nucleotide <- nucleotides, into: %{}, do: {nucleotide, 0}
   end
 end
