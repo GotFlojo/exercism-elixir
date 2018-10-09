cbe0d6a37e32d203552e06f2e4ee2b4db7595f62
Florian Fischer <gotflojo@users.noreply.github.com>
2018-10-09 20:32:48 +0200

    Add exercise for RNA transcription
    Implemented with simple map but then decided to do error handling

---
+++ rna-transcription/rna_transcription.exs
@@ -0,0 +1,25 @@
+defmodule RNATranscription do
+  @transcription %{?A => ?U, ?C => ?G, ?G => ?C, ?T => ?A}
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> RNATranscription.to_rna('ACTG')
+  'UGAC'
+  """
+  @spec to_rna([char]) :: [char] | {:error, String.t()}
+  def to_rna(dna) do
+    case Enum.reduce_while(dna, [], &(valid_nucleotide(&1, &2))) do
+      [:error|_] -> {:error, "invalid nucleotide"}
+            rna  -> Enum.reverse(rna)
+    end
+  end
+
+  defp valid_nucleotide(n, acc) do
+    case Map.fetch(@transcription, n) do
+      {:ok, rna} -> {:cont, [rna | acc]}
+      :error   -> {:halt, [:error | acc]}
+    end
+  end
+end
